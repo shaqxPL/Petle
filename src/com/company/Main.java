b690c73f82dc630855f26f372848d566493d80b0
shaqxPL <tomasz198720@gmail.com>
2022-09-16 17:20:04 +0200

    Petla: for loop

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for(int i = 10; i>=1; i--){
+        System.out.println("wartość i to: " + i);
+    }
+    }
+}
